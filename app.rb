82bdd2994272f7db0e7c5fa3f5dafadabc687095
Jimmy Hsu <irregular.profit@gmail.com>
2013-12-10 23:52:18 -0800

    updated guest list
    enabled mail
    
    adding some checks for wrong plus_one option

--- app.rb
+++ app.rb
@@ -53,6 +53,7 @@ get '/rsvp' do
   haml :rsvp
 end
 
+
 post '/rsvp_action' do
   puts params.inspect
 
@@ -61,36 +62,43 @@ post '/rsvp_action' do
   guest_name = guest_params.delete('name')
 
   @guest = Guest.find_by_id(guest_id)
-  @guest.attributes = guest_params
-  @guest.rsvp_ed = true
 
-  if @guest.save
-    # Pony.mail({to: 'irregular.profit@gmail.com',
-    #            from: 'irregular.profit@gmail.com',
-    #            subject: "Wedding RRSP: #{@guest.name}",
-    #            body: haml(:email),
-    #            via: :smtp,
-    #            via_options: {
-    #              address:              'smtp.gmail.com',
-    #              port:                 '465',
-    #              enable_starttls_auto: true,
-    #              user_name:            'irregular.profit',
-    #              password:             'uqjseayluuzmoojy',
-    #              authentication:       :plain,
-    #              domain:               "localdomain"
-    # }})
-
-    redirect '/rsvp', notice: 'Thank you for RSVPing! We look forward to seeing you.'
+  if @guest.name != guest_name || (guest_params['has_plus_one'] && guest_params['has_plus_one'] == 'on' && !@guest.can_plus_one)
+    redirect '/rsvp', error: "We are sorry. Something is wrong. Would you select your name from the dropdown and resubmit please?"
   else
-    redirect '/rsvp', error: "Something is wrong :(. We weren't able to RSVP you. Please call Jimmy."
+    @guest.attributes = guest_params
+    @guest.rsvp_ed = true
+
+    if @guest.save
+      @num_unconfimed_guests = Guest.where(rsvp_ed: false).count
+      Pony.mail({to: 'irregular.profit@gmail.com',
+                 from: 'irregular.profit@gmail.com',
+                 subject: "Wedding RRSP: #{@guest.name}",
+                 html_body: haml(:email),
+                 via: :smtp,
+                 via_options: {
+                   address:              'smtp.gmail.com',
+                   port:                 '587',
+                   enable_starttls_auto: true,
+                   user_name:            'irregular.profit',
+                   password:             'uqjseayluuzmoojy',
+                   authentication:       :plain,
+                   domain:               "localdomain"
+      }})
+      first_name = @guest.name.split(' ').first
+      redirect '/rsvp', notice: "Thank you for RSVPing #{first_name}! We look forward to seeing you."
+    else
+      redirect '/rsvp', error: "Something is wrong :(. We weren't able to RSVP you. Please call Jimmy."
+    end
   end
+
 end
 
 get '/guests.?:format?' do
   content_type :json
 
   puts params.inspect
-  @guests = Guest.as_you_type(params[:guest_name]).collect{|x| [name: x.name, id: x.id, plus_one: x.can_plus_one]}
+  @guests = Guest.where(rsvp_ed: false).as_you_type(params[:guest_name]).collect{|x| [name: x.name, id: x.id, plus_one: x.can_plus_one]}
 
   {matchingGuests: @guests.flatten}.to_json
 
