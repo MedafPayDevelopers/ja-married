2b43aa3ada498d8579ff046c6d8654895de7eaf5
Jimmy Hsu <irregular.profit@gmail.com>
2014-02-05 22:33:00 -0800

    fix

--- config/environments.rb
+++ config/environments.rb
@@ -1,5 +1,6 @@
 require 'uri'
 
+
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
